917937708b4ad095614630403380285fd815c151
mateus-souza03 <80261495+mateus-souza03@users.noreply.github.com>
2023-04-14 19:44:36 -0300

    NewRepository
    - Criando outro projeto, pois o outro estava dando muito erro

---
+++ ControleEstoque.Web/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ControleEstoque.Web.Models;
+
+namespace ControleEstoque.Web
+{
+    public static class AutoMapperProfile
+    {
+        public static IMapper CreateConfig()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<GrupoProdutoViewModel, GrupoProdutoModel>().ReverseMap();
+                cfg.CreateMap<MarcaProdutoViewModel, MarcaProdutoModel>().ReverseMap();
+                cfg.CreateMap<PerfilViewModel, PerfilModel>().ReverseMap();
+                cfg.CreateMap<MarcaProdutoViewModel, MarcaProdutoModel>().ReverseMap();
+                cfg.CreateMap<UsuarioViewModel, UsuarioModel>().ReverseMap();
+            }).CreateMapper();
+
+            return config;
+        }
+    }
+}
---
+++ ControleEstoque.Web/Models/ContextoBD.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+
+namespace ControleEstoque.Web.Models
+{
+    public class ContextoBD : DbContext
+    {
+        public ContextoBD() : base("name=principal")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new GrupoProdutoMap());
+            modelBuilder.Configurations.Add(new MarcaProdutoMap());
+            modelBuilder.Configurations.Add(new PerfilMap());
+            modelBuilder.Configurations.Add(new UsuarioMap());
+        }
+
+        public DbSet<GrupoProdutoModel> GruposProdutos { get; set; }
+        public DbSet<MarcaProdutoModel> MarcasProdutos { get; set; }
+        public DbSet<PerfilModel> PerfisUsuarios { get; set; }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
+    }
+}
